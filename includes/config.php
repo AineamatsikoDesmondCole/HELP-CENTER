3cb665599b61f905379e78148cf20e8de3c96d7f
AineamatsikoDesmondCole <desmondrosscole@gmail.com>
2026-02-04 10:21:37 +0300

    Update config.php Email secret

--- includes/config.php
+++ includes/config.php
@@ -13,8 +13,9 @@ define('SITE_URL', 'http://localhost/erp-help-center/');
 define('SESSION_TIMEOUT', 3600); // 1 hour
 
 define('SMTP_HOST', 'smtp.gmail.com');
-define('SMTP_USER', 'desmondrosscole@gmail.com');
-define('SMTP_PASS', 'dexnmgbjacvhjuxa');
+define('SMTP_USER', '');
+define('SMTP_PASS', '');
 define('SMTP_PORT', 587);
+
 
 ?>
